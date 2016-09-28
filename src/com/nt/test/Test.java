ac96c58915c02770c6bc4b2a3a6cf634e1682819
fajruddinsk <fajruddinsk111@gmail.com>
2016-09-28 13:27:44 +0530

    test class modify

--- src/com/nt/test/Test.java
+++ src/com/nt/test/Test.java
@@ -7,7 +7,9 @@ public class Test {
 
 	public static void main(String[] args) {
 	System.out.println("hi world");
- List al=new ArrayList();
+ List<String> al=new ArrayList<String>();
+ al.add("hi");
+ 
  
 	}
 
