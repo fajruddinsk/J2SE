5e2514660dead362c7c0e405edcfa7d81531d36b
fajruddinsk <fajruddinsk111@gmail.com>
2016-09-28 13:08:50 +0530

    Inatial commit

---
+++ src/com/nt/test/Test.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+	System.out.println("hi world");
+ List al=new ArrayList();
+ 
+	}
+
+}
